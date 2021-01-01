f426dd32066ba90450ade251fcee6551c7ec2517
Ashwith Poojary <pashwith25@gmail.com>
2021-01-01 21:06:11 +0530

    final

--- fileOrganizer.py
+++ fileOrganizer.py
@@ -1,10 +1,7 @@
 import os
 import subprocess
-import platform
 
 try:
-    system = platform.system()
-
     print("Your Current working directory is:", os.getcwd()) #checks your current working directory
     try:
         os.mkdir("SortedDownloads") #creates a new directory if not already present
@@ -15,28 +12,22 @@ try:
 
     sourceDirectory = "/home/ashwith/Downloads"
     destinationDirectory = "/home/ashwith/SortedDownloads"
-
-    def Automation(command):
-        while 1:
-            for files in os.listdir(sourceDirectory):
-                filepath = os.path.join(sourceDirectory, files)
+    while 1:
+        for files in os.listdir(sourceDirectory):
+            filepath = os.path.join(sourceDirectory, files)
+            try:
+                os.chdir(filepath)
+                subprocess.run(["mv", filepath, destinationDirectory])
+            except:
+                extension = files.split(".")[1]
                 try:
-                    os.chdir(filepath)
-                    subprocess.run([command, filepath, destinationDirectory])
+                    os.chdir(destinationDirectory)
+                    os.mkdir(extension)
+                    tempPath = os.path.join(destinationDirectory, extension)
+                    subprocess.run(["mv", filepath, tempPath])
                 except:
-                    extension = files.split(".")[1]
-                    try:
-                        os.chdir(destinationDirectory)
-                        os.mkdir(extension)
-                        tempPath = os.path.join(destinationDirectory, extension)
-                        subprocess.run([command, filepath, tempPath])
-                    except:
-                        tempPath = os.path.join(destinationDirectory, extension)
-                        subprocess.run([command, filepath, tempPath])
-
-    if system.lower!="windows":
-        Automation("mv")
-    else:
-        Automation("move")
+                    tempPath = os.path.join(destinationDirectory, extension)
+                    subprocess.run(["mv", filepath, tempPath])
+                    
 except KeyboardInterrupt:
     print("\nexited")
